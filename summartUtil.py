ca2dafc66d49e5e2f8b4bf3deda5b3c83d8509d9
Kevin Cheung <kc426@drexel.edu>
2010-09-01 03:36:39 -0400

    Added getSentences() to return a list of sentences

--- summartUtil.py
+++ summartUtil.py
@@ -20,6 +20,20 @@ def getHtmlText(url):
 	s = f.read()
 	f.close()
 	return html2txt(s)
+
+##
+# @param text the entire article
+# @return a list of sentences
+def getSentences(text):
+	text = text.replace('\n', '')
+	lst = re.split(r"([\.\?\!\s]+)", text)
+	sentences = []
+	for i in range(0, len(lst), 2):
+		if i+1 < len(lst):
+			sentences.append(lst[i] + lst[i+1])
+		else:
+			sentences.append(lst[i])
+	return sentences
 	
 def html2txt(s, hint = 'entity', code = 'ISO-8859-1'):
 	"""Convert the html to raw txt

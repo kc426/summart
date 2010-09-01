bd6714dae25761bac93475e801f539007fea82e6
Kevin Cheung <kc426@drexel.edu>
2010-08-31 22:30:00 -0400

    Allow to input URL or plain text

--- summart.py
+++ summart.py
@@ -3,17 +3,33 @@
 
 import summartUtil
 import markov
+import sys
 
-DEBUG = True
+DEBUG = False 
 
 def main():
+	text = ""
+	option = int(raw_input("1. URL, 2. plain text:"))
+	if option == 1:
+		if DEBUG:
+			url = "http://www.cs.drexel.edu/~jpopyack/Kasparov.html"
+		else:
+			url = raw_input("Enter the URL: ")
+		text = summartUtil.getHtmlText(url)
+	elif option == 2:
+		filename = raw_input("Enter the filename: ")
+		fin = file( filename )
+		for line in fin:
+			 text += line + '\n'
+		fin.close()
+	else:
+		print "Please enter the right option"
+		sys.exit(2)
+
 	if DEBUG:
-		url = "http://www.cs.drexel.edu/~jpopyack/Kasparov.html"
 		MAXGEN = 200
 	else:
-		url = raw_input("Enter the URL: ")
 		MAXGEN = int(raw_input("Enter number of words you want to summarize: "))
-	text = summartUtil.getHtmlText(url)
 	table = {}
 	markov.build(text.split('\n'), table)
 	markov.generate(table, MAXGEN)

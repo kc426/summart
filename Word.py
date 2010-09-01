7d77cb7faf229711f0d51e0fc8cb952589534aed
Kevin Cheung <kc426@drexel.edu>
2010-09-01 08:52:12 -0400

    Modified Word class

--- Word.py
+++ Word.py
@@ -3,35 +3,34 @@
 import random
 
 class Word:
-	# The actual word this is
-	__main_word__ = ""
-	# Times the word was used
-	__usage_count__ = 0
-	# List of related words
-	__related__ = dict()
-	# Related adjectives
-	__adjectives__ = dict()
-	# A list of words that can come before this word
-	__pre__ = dict()
-	# A list of words that can come after this word
-	__post__ = dict()
-
 	# Check if the word is in the dictionary and only add it if it isn't
 	def __add_word__(self, dict, word):
 		if dict.has_key(word.getWord()):
 			return
 		else:
-			dict[word.getWord()] = word
+			self.dict[word.getWord()] = word
 
 	# Return a random word from a dictionary
-	def __get_rand_word__(self, dict):
-		list = dict.values()
+	def __get_rand_word(self, dict):
+		list = self.dict.values()
 		element = random.randint(0, len(list))
-		return self.list[element]
+		return list[element]
 		
 	def __init__(self, main_word):
 		random.seed()
 		self.__main_word__ = main_word
+		# The actual word this is
+		self.__main_word__ = ""
+		# Times the word was used
+		self.__usage_count__ = 0
+		# List of related words
+		self.__related__ = {}
+		# Related adjectives
+		self.__adjectives__ = {}
+		# A list of words that can come before this word
+		self.__pre__ = {}
+		# A list of words that can come after this word
+		self.__post__ = {}
 
 	def __eq__(self, other):
 		if self.__main_word__ == other.getWord():
@@ -58,23 +57,23 @@ class Word:
 		self.__add_word__(self.__related__, word)
 
 	def getRelated(self):
-		return self.__get_rand_word__(self.__related__)
+		return self.__get_rand_word(self.__related__)
 
 	def addAdjective(self, word):
 		self.__add_word__(self.__adjectives__, word)
 
 	def getAdjective(self):
-		return self.__get_rand_word__(self.__adjectives__)
+		return self.__get_rand_word(self.__adjectives__)
 
 	def addPostWord(self, word):
 		self.__add_word__(self.__post__, word)
 
 	def getPostWord(self):
-		return self.__get_rand_word__(self.__post__)
+		return self.__get_rand_word(self.__post__)
 
 	def addPreWord(self, word):
 		self.__add_word__(self.__pre__, word)
 
 	def getPreWord(self):
-		return self.__get_rand_word__(self.__pre__)
+		return self.__get_rand_word(self.__pre__)
 

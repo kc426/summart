b0c423f96be2b836ba0f80bc73291a840ba62423
Kevin Cheung <kc426@drexel.edu>
2010-09-03 19:04:45 -0400

    Fixed getSentences(), strip out punctuation

--- summartUtil.py
+++ summartUtil.py
@@ -25,14 +25,10 @@ def getHtmlText(url):
 # @param text the entire article
 # @return a list of sentences
 def getSentences(text):
-	text = text.replace('\n', '')
-	lst = re.split(r"([\.\?\!]+\s?)", text)
-	sentences = []
-	for i in range(0, len(lst), 2):
-		if i+1 < len(lst):
-			sentences.append(lst[i] + lst[i+1])
-		else:
-			sentences.append(lst[i])
+	endline = re.compile("(\w) *(\n)([A-Z])")
+	text = endline.sub(r"\1.\2\3", text)
+	text = re.sub(r"\n", '', text)
+	sentences = re.split(r"[\.\?\!]+\s?", text)
 	return sentences
 	
 def html2txt(s, hint = 'entity', code = 'ISO-8859-1'):
